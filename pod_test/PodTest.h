5a157db481ced391846474512a20ca96cfb10f86
罗良 <luoliang@luoliangdeiMac.local>
2017-01-20 15:31:01 +0800

    hehe

---
+++ pod_test/PodTest.h
@@ -0,0 +1,14 @@
+//
+//  PodTest.h
+//  pod_test
+//
+//  Created by 罗良 on 2017/1/20.
+//  Copyright © 2017年 罗良. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodTest : NSObject
++(void)test;
+
+@end
---
+++ pod_test/PodTest.m
@@ -0,0 +1,16 @@
+//
+//  PodTest.m
+//  pod_test
+//
+//  Created by 罗良 on 2017/1/20.
+//  Copyright © 2017年 罗良. All rights reserved.
+//
+
+#import "PodTest.h"
+
+@implementation PodTest
++(void)test{
+    NSLog(@"test");
+}
+
+@end
